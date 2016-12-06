70efbc53bed3fece98b87b685734510d78b85fa6
wellington.siqueira <wellingtonex@gmail.com>
2016-12-06 18:23:06 -0300

    criando rota para cancelar um pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -58,5 +58,26 @@ module.exports = (app) => {
             res.send(pagamento);
         });
 
+
+    });
+
+    app.delete('/pagamentos/pagamento/:id', (req, res) => {
+        let id = req.params.id;
+        let pagamento = {};
+
+        pagamento.id = id;
+        pagamento.status = 'CANCELADO';
+
+        let connection = app.persistencia.connectionFactory();
+        let pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+         pagamentoDao.atualiza(pagamento, (erro) => {
+            if(erro) {
+                res.status(500).send(erro);
+                return;
+            }
+            res.status(204).send(pagamento);
+        });
+
     });
 }
